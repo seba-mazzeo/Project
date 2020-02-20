3399af91f3f8f600e043964b2715e50651b2187e
¨Sebastian <smazzeo@seger-it.com>
2020-02-20 09:25:31 -0300

    UniTest and FT pass, end of 7 chapter

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -95,21 +95,21 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis visits the home page, There is  no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body'.text)
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item. He
         # is less interesting thean Edith...
-        inputbox = self.browser.find_element_by+id('id_new_item')
-        inputbox.send_keys('Buy mik')
-        inputbox.send_keys(Key.ENTER)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         
         # Francis gets his own unique URLD
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
-        selfassertNotEqual(francis_list_url, edith_list_url)
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, thereis no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
