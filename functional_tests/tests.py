54465db082790c61bba52a719f3abd16fb2a9385
¨Sebastian <smazzeo@seger-it.com>
2020-02-21 09:45:05 -0300

    Adding static files and switching to StaticLiveServerTestCase, FT pass :)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.test import StaticLiveServerTestCase
 import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
